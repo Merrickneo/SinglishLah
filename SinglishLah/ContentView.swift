af8d28c364462e4f4935d8a9703922a480a55009
Merrickneo <merrickneo@gmail.com>
2022-05-12 17:44:56 +0800

    Initial Commit

---
+++ SinglishLah/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SinglishLah
+//
+//  Created by Merrick Neo on 12/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SinglishLah/SinglishLahApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglishLahApp.swift
+//  SinglishLah
+//
+//  Created by Merrick Neo on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SinglishLahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
