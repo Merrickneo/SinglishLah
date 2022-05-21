bc0800e8d19bb1617289f2d434d1c9d9413c8c2c
Merrickneo <merrickneo@gmail.com>
2022-05-22 00:27:35 +0800

    Touched up Registration Page

--- SinglishLah/SinglishLahApp.swift
+++ SinglishLah/SinglishLahApp.swift
@@ -6,12 +6,77 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+
+class AppViewModel: ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    @Published var signedIn = false
+    
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) {
+            [weak self] result, error in
+            
+            guard result != nil, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                // Success
+                self?.signedIn = true
+            }
+            
+        }
+    }
+    
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) {
+            [weak self] result, error in
+            
+            guard result != nil, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                // Success
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signOut() {
+        try? auth.signOut()
+        
+        self.signedIn = false
+    }
+}
+
+
 
 @main
 struct SinglishLahApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
+            let viewModel = AppViewModel()
             RegistrationPageView()
+                .environmentObject(viewModel)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+         [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
     }
 }
