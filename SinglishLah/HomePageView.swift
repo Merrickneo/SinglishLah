5d98c938d2197d0faf9abba91f67f1405f369388
Ryu Suzuki <ryu1507@yahoo.com>
2022-05-25 14:41:24 +0800

    Homepage 1

---
+++ SinglishLah/HomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageView.swift
+//  SinglishLah
+//
+//  Created by Ryu Suzuki on 23/5/22.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
---
+++ SinglishLah/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SinglishLah
+//
+//  Created by Ryu Suzuki on 24/5/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
