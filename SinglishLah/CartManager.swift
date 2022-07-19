658e25bb312d8bb6787706f6b6f4af6a76470f3b
Merrickneo <merrickneo@gmail.com>
2022-07-19 18:18:15 +0800

    Update translation branch

--- SinglishLah/CartManager.swift
+++ SinglishLah/CartManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+<<<<<<< HEAD
+=======
+
+class CartManager: ObservableObject {
+    @Published private(set) var vouchers: [Voucher] = []
+    @Published private(set) var total: Int = 0
+    
+    func addToCart(voucher: Voucher) {
+        vouchers.append(voucher)
+        total += voucher.amount
+    }
+    
+    func removeFromCart(voucher: Voucher) {
+        var index: Int?
+        for i in stride(from: 0, to: vouchers.count, by: 1) {
+            if vouchers[i].amount == voucher.amount {
+                index = i
+            }
+        }
+        if let index = index {
+            vouchers.remove(at: index)
+            total -= voucher.amount
+        }
+    }
+    
+    func removeAllVouchers() {
+        vouchers.removeAll()
+    }
+}
+>>>>>>> main
